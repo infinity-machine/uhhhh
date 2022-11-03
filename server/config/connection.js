7188ec71519ddaebd5e2ef777aca0670f64c753f
Connor Carciofini <ccarciofini@gmail.com>
2022-11-03 00:24:29 -0500

    fixed connect issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const URL = process.env.ATLAS_CONNECT_URL ? process.env.ATLAS_CONNECT_URL : 'mongodb://localhost:27017/weatherbot';
+const URL = process.env.ATLAS_CONNECT_URL ? process.env.ATLAS_CONNECT_URL : 'mongodb://localhost:27017/chat_db';
 
-// Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://localhost:27017/chat_db', {
+mongoose.connect(URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err) => {
